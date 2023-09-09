ccd4175541159a229292cc3b509b867caa53956b
jesse <jessemhan@proton.me>
2023-09-08 23:40:00 -0700

    wip

--- rift-engine/rift/ir/custom_parsers.py
+++ rift-engine/rift/ir/custom_parsers.py
@@ -1,8 +1,14 @@
 from tree_sitter import Language, Parser
-Language.build_library(
-    'build/tree-sitter-languages.so',
-    ['vendor/tree-sitter-rescript'])
+import os
+from rift.util.fs import RIFT_PROJECT_DIR
 
-ReScript = Language('build/tree-sitter-languages.so', 'rescript')
+TREE_SITTER_LANGUAGES_PATH = os.path.join(RIFT_PROJECT_DIR, "build", "tree-sitter-languages.so")
+VENDOR_PATH = os.path.join(RIFT_PROJECT_DIR, "vendor")
+
+Language.build_library(
+    TREE_SITTER_LANGUAGES_PATH,
+    [os.path.join(VENDOR_PATH, 'tree-sitter-rescript')])
+
+ReScript = Language(TREE_SITTER_LANGUAGES_PATH, 'rescript')
 
 parser: Parser = Parser()
--- rift-engine/rift/ir/rescript_parser.py
+++ rift-engine/rift/ir/rescript_parser.py
@@ -1,7 +1,13 @@
 from tree_sitter import Language, Parser
-Language.build_library('build/tree-sitter-languages.so', [ 'vendor/tree-sitter-lean' ] )
+import os
+from rift.util.fs import RIFT_PROJECT_DIR
 
-LEAN_LANGUAGE = Language('build/tree-sitter-languages.so', 'lean')
+TREE_SITTER_LANGUAGES_PATH = os.path.join(RIFT_PROJECT_DIR, "build", "tree-sitter-languages.so")
+VENDOR_PATH = os.path.join(RIFT_PROJECT_DIR, "vendor")
+
+Language.build_library(TREE_SITTER_LANGUAGES_PATH, [ os.path.join(VENDOR_PATH, 'tree-sitter-lean') ] )
+
+LEAN_LANGUAGE = Language(TREE_SITTER_LANGUAGES_PATH, 'lean')
 
 parser : Parser = Parser()
 parser.set_language(LEAN_LANGUAGE)
